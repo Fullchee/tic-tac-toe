ac695f3b374dc0bbe039a81cd5d55841cb58eb0d
Fullchee Zhang <fullchee@gmail.com>
2019-01-21 09:53:32 -0500

    Updates

---
+++ precache-manifest.f71f49595a2f6a709b67b31a4e45aa4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "c6760f804aefbb0915e8",
+    "url": "/tic-tac-toe/static/js/main.c6760f80.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "c6760f804aefbb0915e8",
+    "url": "/tic-tac-toe/static/css/main.70b86423.chunk.css"
+  },
+  {
+    "revision": "ff72ced1b53a5947c53cf0135c491bc2",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
